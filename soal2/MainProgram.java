d9ca84250ac3726d579a928400bbb47317ac2a73
amelianathasa <amelia.nathasa.tif421@polban.ac.id>
2022-03-31 12:17:10 +0700

    first commit

---
+++ soal2/MainProgram.java
@@ -0,0 +1,20 @@
+package soal2;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shape s1 = new Shape();
+		 System.out.println(s1.toString());
+		 
+		 Circle c1 = new Circle(); 
+		 System.out.println(c1.toString());
+		 
+		 Rectangle r1 = new Rectangle(); 
+		 System.out.println(r1.toString());
+		 
+		 Square sq1 = new Square(); 
+		 System.out.println(sq1.toString());
+	}
+
+}
